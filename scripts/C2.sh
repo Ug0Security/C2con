5cea416cb23113d5c7828eecca5bb6fd806dac3d
Ug0Security <c.vallicari@gmail.com>
2022-10-11 17:29:23 +0200

    Update C2.sh

--- scripts/C2.sh
+++ scripts/C2.sh
@@ -3,8 +3,7 @@ tmux new-session -s ses-0 -n my-screen-name -d
 tmux split-window -v
 tmux split-window -h
 #tmux select-pane -t:.0 -P "bg=#7A306C"
-tmux send-keys -t 0 "bash Run_Cmd.sh" C-m
 tmux send-keys -t 1 "tail -f /var/www/html/res.txt" C-m
 tmux send-keys -t 2 "while true; do cat /var/www/html/ping.txt; sleep 5; clear; done" C-m
+tmux send-keys -t 0 "bash Run_Cmd.sh" C-m
 tmux attach-session -t ses-0
-
