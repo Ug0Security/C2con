109c6491f7e24216ad7a6170280e2f4c03321b03
Ug0Security <c.vallicari@gmail.com>
2022-10-12 21:35:14 +0200

    Update C2.sh

--- scripts/C2.sh
+++ scripts/C2.sh
@@ -4,7 +4,7 @@ tmux split-window -v
 tmux split-window -h
 tmux send-keys -t 1 "tail -f /var/www/html/res.txt" C-m
 tmux send-keys -t 2 "while true; do cat /var/www/html/ping.txt; sleep 5; clear; done" C-m
-tmux send-keys -t 0 "bash Run_Cmd.sh" C-m
+tmux send-keys -t 0 "echo 'Welcome to Nowhere'" C-m
 tmux select-pane -t 0
 tmux attach-session -t C2con-sess
 
