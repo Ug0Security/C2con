7652a78e6ce4c658908e12e9a7f9c1659e194054
Ug0Security <c.vallicari@gmail.com>
2022-10-15 21:59:29 +0200

    Update create_listener.sh

--- scripts/create_listener.sh
+++ scripts/create_listener.sh
@@ -3,7 +3,7 @@ sed -i "s/MODE/$2/" $1
 sed -i "s/RESPONSE/$3/" $1
 cp $1 /var/www/html/$1
 rm $1
-echo "Listener $1 deployed"
+echo "Listener $1 deployed to /var/www/html/$1"
 echo """
 ---CONFIG---
 Endpoint: $1
