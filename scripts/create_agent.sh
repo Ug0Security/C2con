f9269533b4474102ce4a4d8b047144c782fafc84
Ug0Security <c.vallicari@gmail.com>
2022-10-15 20:06:22 +0200

    Update create_agent.sh

--- scripts/create_agent.sh
+++ scripts/create_agent.sh
@@ -29,6 +29,22 @@ Endpoint:$3
 Mode Param: $4
 Response Param: $5
 """
+
+elif [ $1 == "go" ]; then
+cp agents-templates/template.go agents-templates/$2
+sed -i "s/ENDPOINT/$3/gi" agents-templates/$2
+sed -i "s/MODE/$4/gi" agents-templates/$2
+sed -i "s/RESPONSE/$5/gi" agents-templates/$2
+cp agents-templates/$2 /tmp/$2
+rm agents-templates/$2
+echo "Agent saved at /tmp/$2"
+echo """
+----Config----
+Endpoint:$3
+Mode Param: $4
+Response Param: $5
+"""
+
 else
 echo "No Agent of this type"
 fi
