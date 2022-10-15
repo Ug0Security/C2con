7968d997c66768355caef4aa9da96c79b2a559fc
Ug0Security <c.vallicari@gmail.com>
2022-10-15 19:39:02 +0200

    Create create_listener.sh

---
+++ scripts/create_listener.sh
@@ -0,0 +1,12 @@
+cp listener-templates/template.php $1
+sed -i "s/MODE/$2/" $1
+sed -i "s/RESPONSE/$3/" $1
+cp $1 /var/www/html/$1
+rm $1
+echo "Listener $1 deployed"
+echo """
+---CONFIG---
+Endpoint: $1
+Mode Param : $2
+Response Param : $3
+"""
