14704ca5159d0459a9bb7b09299db60eda84cc61
Ug0Security <c.vallicari@gmail.com>
2022-10-15 19:51:41 +0200

    Update create_agent.sh

--- scripts/create_agent.sh
+++ scripts/create_agent.sh
@@ -1,13 +1,34 @@
-cp agents-templates/template.sh agents-templates/$1
-sed -i "s/ENDPOINT/$2/gi" agents-templates/$1
-sed -i "s/MODE/$3/gi" agents-templates/$1
-sed -i "s/RESPONSE/$4/gi" agents-templates/$1
-cp agents-templates/$1 /tmp/$1
-rm agents-templates/$1
-echo "Agent saved at /tmp/$1"
+
+if [ $1 == "shell" ]; then
+echo "Building $1 agent"
+cp agents-templates/template.sh agents-templates/$2
+sed -i "s/ENDPOINT/$3/gi" agents-templates/$2
+sed -i "s/MODE/$4/gi" agents-templates/$2
+sed -i "s/RESPONSE/$5/gi" agents-templates/$2
+cp agents-templates/$2 /tmp/$2
+rm agents-templates/$2
+echo "Agent saved at /tmp/$2"
 echo """
 ----Config----
-Endpoint:$2
-Mode Param: $3
-Response Param: $4
+Endpoint:$3
+Mode Param: $4
+Response Param: $5
 """
+
+elif [ $1 == "python" ]; then
+cp agents-templates/template.py agents-templates/$2
+sed -i "s/ENDPOINT/$3/gi" agents-templates/$2
+sed -i "s/MODE/$4/gi" agents-templates/$2
+sed -i "s/RESPONSE/$5/gi" agents-templates/$2
+cp agents-templates/$2 /tmp/$2
+rm agents-templates/$2
+echo "Agent saved at /tmp/$2"
+echo """
+----Config----
+Endpoint:$3
+Mode Param: $4
+Response Param: $5
+"""
+else
+echo "No Agent of this type"
+fi
