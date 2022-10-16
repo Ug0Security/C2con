f23b40c50d2ac123c7d19e91f64cd0d49f45f3d2
Ug0Security <c.vallicari@gmail.com>
2022-10-16 15:37:32 +0200

    Update create_listener.sh

--- scripts/create_listener.sh
+++ scripts/create_listener.sh
@@ -6,6 +6,7 @@ sed -i "s/RESPONSE/$4/" $2
 cp $2 /var/www/html/$2
 rm $2
 echo "Listener $2 deployed to /var/www/html/$2"
+echo " Don't forget to start Apache service"
 echo """
 ---CONFIG---
 Endpoint: $2
@@ -13,6 +14,9 @@ Mode Param : $3
 Response Param : $4
 Exposed on Apache port
 """
+sleep2
+clear
+
 elif [ $1 == "python" ]; then
 mkdir /tmp/$2
 cp -r listener-templates/pylistener/ /tmp/$2/
@@ -27,6 +31,10 @@ Response Param : $4
 Exposed on Port : $5
 """
 python /tmp/$2/pylistener/manage.py runserver 0.0.0.0:$5 > /dev/nul 2>&1 
+sleep2
+clear
 else
-echo "No Agent of this type"
+echo "No Listener of this type"
+sleep2
+clear
 fi
