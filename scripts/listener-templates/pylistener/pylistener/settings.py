05a86982ba8133006c0ebd9a882ad729ed84fb1d
Ug0Security <c.vallicari@gmail.com>
2022-10-17 16:00:13 -0400

    Update settings.py

--- scripts/listener-templates/pylistener/pylistener/settings.py
+++ scripts/listener-templates/pylistener/pylistener/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h7g=#8)$$t6*@=%$_1983t0yjejufe4n(*=&1^l+aijzt@7jl('
+SECRET_KEY = 'TotallyNotALeaklfkjglskqmfdmqeznqefnflmkgfdsmlgqezrTotallyNotALeak'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
