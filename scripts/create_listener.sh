96b02851fb91c4b844ad35101b1add3fb776b159
Ug0Security <c.vallicari@gmail.com>
2022-12-15 18:48:29 +0100

    Update create_listener.sh

--- scripts/create_listener.sh
+++ scripts/create_listener.sh
@@ -15,7 +15,7 @@ Response Param : $4
 Exposed on Apache port
 """
 sleep 2
-clear
+
 
 elif [ $1 == "python" ]; then
 mkdir /tmp/$2
@@ -35,9 +35,9 @@ Exposed on Port : $5
 python /tmp/$2/pylistener/manage.py runserver 0.0.0.0:$5 > /dev/nul 2>&1 &
 sleep 2
 echo "Listener python on 0.0.0.0:$5 (PID: $(netstat -laputen | grep 0.0.0.0:$5 | cut -d ' '  -f 65 | cut -d '/' -f 1))" >> /tmp/C2CON-listener.txt
-clear
+
 else
 echo "No Listener of this type"
 sleep 2
-clear
+
 fi
